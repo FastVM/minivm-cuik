c838732f490fa6fe2ffa9d643396c06627f43df7
Shaw Summa <shawsumma@gmail.com>
2024-04-23 05:54:04 -0400

    print c update

--- tb/src/opt/print_c.h
+++ tb/src/opt/print_c.h
@@ -1071,7 +1071,7 @@ static void c_fmt_bb(CFmtState* ctx, TB_Worklist* ws, TB_Node* bb_start) {
                 TB_Node *ptr = n->inputs[n->input_count-1];
 
                 c_fmt_output(ctx, n);
-                nl_buffer_format(ctx->buf, "(void*) ((char *) ");
+                nl_buffer_format(ctx->buf, "(void*) ((size_t)");
                 c_fmt_ref_to_node(ctx, ptr);
                 nl_buffer_format(ctx->buf, " + (size_t) ");
                 c_fmt_ref_to_node(ctx, off);
